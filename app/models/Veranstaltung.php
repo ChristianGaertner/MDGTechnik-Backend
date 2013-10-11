ab0209e32faf4be1951e20b1b47a90ff24e85458
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-10-11 13:34:43 +0200

    Removed testing dummy data from model

--- app/models/Veranstaltung.php
+++ app/models/Veranstaltung.php
@@ -15,38 +15,4 @@ class Veranstaltung extends Eloquent {
 	 * @var array
 	 */
 	protected $hidden = array('key');
-
-	public static function getSingle($id) {
-		return array(
-				"id" => $id,
-				"status" => array(
-					"message" => "ok",
-					"type" => "positive"
-					),
-				"createdAt" => "1 Januar 1971",
-				"lastEdit" => "1 Januar 1971",
-				"name" => "Konzert XYZ",
-				"author" => "Christian Gärtner",
-				"email" => "christiangaertner.film@googlemail.com",
-				"loc" => "Aula",
-				"date" => "1 Januar 1971",
-				"timespan" => "00:00 - 24:00",
-				"req" => "2x Mikrophon",
-				"notes" => "Alles soll blau sein!!",
-				"workers" => array(
-					array(
-						"name" => "Christian",
-						"job" => "Licht"
-						),
-					array(
-						"name" => "Philip",
-						"job" => "Ton"
-						),
-					array(
-						"name" => "Niloy",
-						"job" => "Ton"
-						),
-				)
-			);
-	}
 }
